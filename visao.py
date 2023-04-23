d1c064916d64ab4184add3ed2a877e3fc1c5b7c5
RodrigoZonzin <rodrigojzonzin@gmail.com>
2023-04-22 23:16:02 -0300

    salvando os url

--- visao.py
+++ visao.py
@@ -1,7 +1,16 @@
 import consulta
 import os
+import pandas as pd
+from datetime import date
 
 entrada = 1;
+
+if os.path.exists('urls.csv'):
+    arquivo_urls = pd.read_csv('urls.csv'); 
+else:
+    arquivo_urls = pd.DataFrame(columns=['url', 'date']);
+
+
 while(entrada != 0):
     print("0:Sair \t 1:Adicionar dados \t 2:Consultar dados");
     entrada = int(input("Sua escolha\n"));
@@ -9,6 +18,9 @@ while(entrada != 0):
     if(entrada == 1):
         os.system('clear')
         url = str(input("Copie a URL aqui\n")); 
+        arquivo_urls = arquivo_urls._append({'url': url, 'date': date.today()}, ignore_index = True);
+        arquivo_urls.to_csv('urls.csv', index = False)
+
         escolhe_loja = int(input("Qual foi a loja?\n1-BH 2-Eskynao 3-Rei das Frutas 4-outro\n")); 
         if escolhe_loja == 1: 
             loja = "BH";
@@ -24,6 +36,8 @@ while(entrada != 0):
             print("Adicionado com sucesso!");
         else:
             print("Erro! :(\n");
+
+        
     
     if(entrada == 2):
         os.system('clear')
